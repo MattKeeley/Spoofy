04228d163599665509692134ba8b83a266711009
Matt Keeley <keeley55@me.com>
2024-08-12 15:13:34 -0700

    better formatting

--- test.py
+++ test.py
@@ -3,19 +3,6 @@ from modules.spoofing import Spoofing
 
 
 class TestSpoofy(unittest.TestCase):
-
-    """
-    0: Indicates that spoofing is possible for the domain.
-    1: Indicates that subdomain spoofing is possible for the domain.
-    2: Indicates that organizational domain spoofing is possible for the domain.
-    3: Indicates that spoofing might be possible for the domain.
-    4: Indicates that spoofing might be possible (mailbox dependent) for the domain.
-    5: Indicates that organizational domain spoofing may be possible for the domain.
-    6: Indicates that subdomain spoofing might be possible (mailbox dependent) for the domain.
-    7: Indicates that subdomain spoofing is possible, and organizational domain spoofing might be possible.
-    8: Indicates that spoofing is not possible for the domain.
-    """
-
     """
     --- Format --- 
     domain: str,
@@ -30,111 +17,127 @@ class TestSpoofy(unittest.TestCase):
 
     def test_spoofing_is_possible(self):
         spoofing = Spoofing(
-            "test_0.com", "none", "r", "v=spf1 include:fake.gov", "~all", 3, None, 100
+            domain="test_0.com", 
+            dmarc_record="v=DMARC1; p=none;", 
+            p="none", 
+            aspf="r", 
+            spf_record="v=spf1 include:fake.gov", 
+            spf_all="~all", 
+            spf_dns_queries=3, 
+            sp=None, 
+            pct=100
         )
         self.assertEqual(spoofing.spoofable, 0)
 
     def test_subdomain_spoofing(self):
         spoofing = Spoofing(
-            "test_1.com",
-            "reject",
-            None,
-            "v=spf1 include:fakest.domain.com",
-            "-all",
-            3,
-            "none",
-            None,
+            domain="test_1.com",
+            dmarc_record="v=DMARC1; p=reject;",
+            p="none",
+            aspf=None,
+            spf_record="v=spf1 include:fakest.domain.com",
+            spf_all="-all",
+            spf_dns_queries=3,
+            sp="none",
+            pct=100,
         )
         self.assertEqual(spoofing.spoofable, 1)
 
     def test_organizational_domain_spoofing(self):
         spoofing = Spoofing(
-            "test_2.com",
-            "none",
-            "r",
-            "v=spf1 include:fakest.domain.com include:faker.domain.com",
-            "-all",
-            2,
-            "reject",
-            100,
+            domain="test_2.com",
+            dmarc_record="v=DMARC1; p=none;",
+            p="none",
+            aspf="r",
+            spf_record="v=spf1 include:fakest.domain.com include:faker.domain.com",
+            spf_all="-all",
+            spf_dns_queries=2,
+            sp="reject",
+            pct=100,
         )
         self.assertEqual(spoofing.spoofable, 2)
 
     def test_spoofing_might_be_possible(self):
         spoofing = Spoofing(
-            "test_3.com",
-            "none",
-            None,
-            "v=spf1 include:fakest.domain.com",
-            "~all",
-            1,
-            "quarantine",
-            90,
+            domain="test_3.com",
+            dmarc_record="v=DMARC1; p=none;",
+            p="none",
+            aspf=None,
+            spf_record="v=spf1 include:fakest.domain.com",
+            spf_all="~all",
+            spf_dns_queries=1,
+            sp="quarantine",
+            pct=90,
         )
         self.assertEqual(spoofing.spoofable, 3)
 
     def test_spoofing_might_be_possible_mbd(self):
         spoofing = Spoofing(
-            "test_4.com",
-            "none",
-            None,
-            "v=spf1 include:fakest.domain.com",
-            "-all",
-            1,
-            None,
-            100,
+            domain="test_4.com",
+            dmarc_record="v=DMARC1; p=none;",
+            p="none",
+            aspf=None,
+            spf_record="v=spf1 include:fakest.domain.com",
+            spf_all="-all",
+            spf_dns_queries=1,
+            sp=None,
+            pct=100,
         )
         self.assertEqual(spoofing.spoofable, 4)
 
     def test_org_domain_spoofing_might_be_possible(self):
         spoofing = Spoofing(
-            "test_5.com",
-            "none",
-            None,
-            "v=spf1 include:fakest.domain.com",
-            "-all",
-            1,
-            "reject",
-            100,
+            domain="test_5.com",
+            dmarc_record="v=DMARC1; p=none;",
+            p="none",
+            aspf=None,
+            spf_record="v=spf1 include:fakest.domain.com",
+            spf_all="-all",
+            spf_dns_queries=1,
+            sp="reject",
+            pct=100,
         )
         self.assertEqual(spoofing.spoofable, 5)
 
     def test_subdomain_spoofing_might_be_possible_mbd(self):
         spoofing = Spoofing(
-            "test_6.com",
-            "reject",
-            "r",
-            "v=spf1 include:fakest.domain.com",
-            "?all",
-            1,
-            "none",
-            100,
+            domain="test_6.com",
+            dmarc_record="v=DMARC1; p=reject;",
+            p="reject",
+            aspf="r",
+            spf_record="v=spf1 include:fakest.domain.com",
+            spf_all="?all",
+            spf_dns_queries=1,
+            sp="none",
+            pct=100,
         )
         self.assertEqual(spoofing.spoofable, 6)
 
     def test_subdomain_spoofing_and_org_spoofing_might_be_possible(self):
         spoofing = Spoofing(
-            "test_7.com",
-            "none",
-            None,
-            "v=spf1 include:fakest.domain.com",
-            "~all",
-            3,
-            "none",
-            100,
+            domain="test_7.com",
+            dmarc_record="v=DMARC1; p=none;",
+            p="none",
+            aspf=None,
+            spf_record="v=spf1 include:fakest.domain.com",
+            spf_all="~all",
+            spf_dns_queries=3,
+            sp="none",
+            pct=100,
         )
         self.assertEqual(spoofing.spoofable, 7)
 
     def test_spoofing_not_possible(self):
         spoofing = Spoofing(
-            "test_8.com",
-            "none",
-            "s",
-            "v=spf1 include:domain.com",
-            "-all",
-            1,
-            "reject",
-            100,
+            domain="test_8.com",
+            dmarc_record="v=DMARC1; p=none;",
+            p="none",
+            aspf="s",
+            spf_record="v=spf1 include:domain.com",
+            spf_all="-all",
+            spf_dns_queries=1,
+            sp="reject",
+            pct=100,
         )
         self.assertEqual(spoofing.spoofable, 8)
 
