d437e945b1b0df477823d257576a2e6f04298ab9
Mister-Joe <joseph.tredeau@outlook.com>
2025-07-01 07:43:57 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="Spoofy",
+    version="1.0.2",
+    install_requires=[ "colorama", "dnspython>= 2.2.1", "tldextract", "pandas", "openpyxl" ],
+    entry_points={ "console_scripts": [ "spoofy=spoofy:main" ] }
+)
+
